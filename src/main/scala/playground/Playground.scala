f9a899480ce6098e2675af0c848646716449c033
Pablo Kurskii <pablo.kurskii@gmail.com>
2022-04-30 12:30:50 +0200

    Update Playground.scala

--- src/main/scala/playground/Playground.scala
+++ src/main/scala/playground/Playground.scala
@@ -4,6 +4,14 @@ object Playground {
 
   def main(args: Array[String]): Unit = {
     println("11")
+
+    var generatorScala3 =
+      for
+        n <- List(1, 2, 3)
+        s <- List("s", "d")
+      yield s"$n,$s"
+
+    println(generatorScala3)
   }
 
 }
